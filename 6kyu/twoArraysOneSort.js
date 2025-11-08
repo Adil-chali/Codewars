1aeee696ec7117a9298a0362138c4e9689b9614d
Adil Chali <wert-159@outlook.com>
2025-11-08 18:55:57 +0100

    add twoArraysOneSort solution

---
+++ 6kyu/twoArraysOneSort.js
@@ -0,0 +1,36 @@
+// https://www.codewars.com/kata/546b22225874d24fbd00005b/javascript
+/*Imagine two arrays/lists where elements are linked by their positions in the array. For example:
+
+HowMany = [ 1   ,   6  ,  5  ,   0  ];
+Type = ['house', 'car','pen','jeans'];
+
+Means I have 1 house, 6 cars,5 pens and 0 jeans.
+
+Now if we sort one array we lose the connectivity. The goal is to create a sorting function that keeps the position link linkedSort(arrayToSort,linkedArray,compareFunction). So for every element that moves in arrayToSort(HowMany in the example), the corresponding element in linkedArray(Type in the example) needs to move similarly.
+
+For example in Javascript:
+
+//INPUT
+HowMany = [ 1   ,   6  ,  5  ,   0  ];
+Type = ['house', 'car','pen','jeans'];
+
+//SORT
+res = linkedSort(HowMany,Type,function(a,b){return a-b;})
+
+//OUTPUT
+HowMany === res === [ 0   ,   1   ,  5  ,  6  ];
+Type       ===     ['jeans','house','pen','car'];
+
+linkedSort(...) return the "arrayToSort" sorted only.
+
+If no compare function is provided you should handle like an alphabetical sorting would do, e.g:
+
+[-71,-6,35,0].sort() ===  [-6,-71,0,35] != [-71,-6,0,35]
+
+Note: it is assumed that array are same length. */
+function linkedSort(aToSort, aLinked, compare) {
+  compare = compare || ((a, b) => (''+a).localeCompare(b));
+  let ref = aLinked.slice();
+  aLinked.sort((a, b) => compare(aToSort[ref.indexOf(a)], aToSort[ref.indexOf(b)]));
+  return aToSort.sort((a, b) => compare(a, b));
+}
