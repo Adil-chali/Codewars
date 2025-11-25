447d549aa7fe25fccece7d60c9b54df2dbe90ac6
Adil Chali <wert-159@outlook.com>
2025-11-25 19:07:21 +0100

    add rankVector solution

---
+++ 6kyu/rankVector.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/545f05676b42a0a195000d95/javascript
+// Given an array (or list) of scores, return the array of ranks for each value in the array. The largest value has rank 1, the second largest value has rank 2, and so on. Ties should be handled by assigning the same rank to all tied values. For example:
+
+//     array = [9,3,6,10] --> ranks = [2,4,3,1]
+
+//     array = [3,3,3,3,3,5,1] --> ranks = [2,2,2,2,2,1,7]
+
+//     because there is one 1st place value, a five-way tie for 2nd place, and one in 7th place.
+
+
+function ranks(a) {
+  const sortedArr=a.slice().sort((a,b)=>b-a)
+  const rank =a.map((x)=>sortedArr.indexOf(x)+1)
+  return rank
+}
