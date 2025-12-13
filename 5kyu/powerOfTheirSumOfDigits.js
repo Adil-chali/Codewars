a1efef9004ca3e970e7b22c1538e6a580f61a48b
Adil Chali <wert-159@outlook.com>
2025-12-13 14:33:30 +0100

    add powerOfTheirSumOfDigits solution

---
+++ 5kyu/powerOfTheirSumOfDigits.js
@@ -0,0 +1,25 @@
+// The number 81 has a special property, a certain power of the sum of its digits is equal to 81 (nine squared). Eighty one (81), is the first number in having this property (not considering numbers of one digit). The next one, is 512. Let's see both cases with the details
+
+// 8 + 1 = 9 and 92 = 81
+
+// 512 = 5 + 1 + 2 = 8 and 83 = 512
+
+// We need to make a function that receives a number as argument n and returns the n-th term of this sequence of numbers.
+// Examples (input --> output)
+
+// 1 --> 81
+
+// 2 --> 512
+function powerSumDigTerm(n) {
+ 
+  const specialOnes=[]
+  for(let base=2; base<100;base++){
+    for(let power=2;power<40;power++){
+        const specialOne=Math.pow(base,power)
+        const special=String(specialOne).split("").reduce((a,b)=>Number(a)+Number(b),0)    
+      if(special===base) specialOnes.push(specialOne)
+    }
+  }
+specialOnes.sort((a, b) => a - b);
+  return specialOnes[n-1]
+}
