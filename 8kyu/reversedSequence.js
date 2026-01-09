40013f0705636167a89f2849170f6a1af3f788a1
Adil Chali <wert-159@outlook.com>
2026-01-09 13:43:04 +0000

    add reversedSequence\ solution

--- 8kyu/reversedSequence.js
+++ 8kyu/reversedSequence.js
@@ -1,5 +1,3 @@
-// Description:
-
 // Build a function that returns an array of integers from n to 1 where n>0.
 
 // Example : n=5 --> [5,4,3,2,1]
@@ -9,4 +7,4 @@ const reverseSeq = n => {
    result.push(n-i)
  }
   return result;
-};
+}
