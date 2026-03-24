027890d6be24835cb1e4983e393c31431bb10fa9
adil <wert-159@outlook.com>
2026-03-24 17:56:49 +0000

    Always perfect solution

---
+++ 7kyu/alwaysPerfect.js
@@ -0,0 +1,23 @@
+/* While surfing in web I found interesting math problem called "Always perfect". That means if you add 1 to the product of four consecutive numbers the answer is ALWAYS a perfect square. For example we have: 1,2,3,4 and the product will be 1X2X3X4=24. If we add 1 to the product that would become 25, since the result number is a perfect square the square root of 25 would be 5.
+
+So now lets write a function which takes numbers separated by commas in string format and returns the number which is a perfect square and the square root of that number.
+
+If string contains other characters than number or it has more or less than 4 numbers separated by comma function returns "incorrect input".
+
+If string contains 4 numbers but not consecutive it returns "not consecutive".*/
+function checkRoot(string){
+ const splitStr=string.split(",")
+   if (splitStr.length !== 4 || splitStr.some(x => isNaN(x))) {
+    return 'incorrect input';
+  }
+  const nums = splitStr.map(Number);
+  if (nums.some((x, i) => i < 3 && nums[i+1] - x !== 1)) {
+    return 'not consecutive';
+  }
+const productOfFourPlusOne=  splitStr.reduce((acc,cur)=>acc*cur,1)+1
+if(splitStr.length !== 4 ||splitStr.some(x=>x===isNaN(x))){
+  return 'incorrect input';
+}
+const squareRoot =Math.sqrt(productOfFourPlusOne)
+return `${productOfFourPlusOne}, ${squareRoot}`
+}
